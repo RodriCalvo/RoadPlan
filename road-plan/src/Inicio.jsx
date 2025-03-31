9e7a4d2ab24198107e768741577a151ea6a868f7
RodriCalvo <rodrigo_javiercalvo@hotmail.com>
2025-03-30 23:50:49 -0300

    solcuionando problema

--- road-plan/src/Inicio.jsx
+++ road-plan/src/Inicio.jsx
@@ -12,4 +12,3 @@ const Inicio = () => {
 };
 
 export default Inicio;
-
--- road-plan/src/index.jsx
+++ road-plan/src/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/RoadPlan/road-plan/">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
